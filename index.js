c393499c35857536027eb4a9610e3c7ee0d4b0cf
Movindu Weerabahu <peshalaweerabahu0@gmail.com>
2025-07-08 20:37:53 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+/* hamburger menu function */
+    const hamburger = document.getElementById('hamburger');
+    const navLinks = document.getElementById('nav-links');
+
+    hamburger.addEventListener('click', () => {
+      navLinks.classList.toggle('active');
+    });
