c7559f70653871b706acceda19ffefcfebf3ead5
movindu477 <peshalaweerabahu0@gmail.com>
2025-07-09 15:05:04 +0530

    ok

--- index.js
+++ index.js
@@ -5,3 +5,15 @@
     hamburger.addEventListener('click', () => {
       navLinks.classList.toggle('active');
     });
+
+
+/* section 1 function */
+const logo = document.getElementById("logo");
+
+logo.addEventListener("mouseenter", () => {
+  logo.style.transform = "scale(1.05)";
+});
+
+logo.addEventListener("mouseleave", () => {
+  logo.style.transform = "scale(1)";
+});
